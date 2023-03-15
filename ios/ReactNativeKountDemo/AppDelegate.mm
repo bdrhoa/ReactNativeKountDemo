d955b1c1bd2df7fbb015d38c7c44a02d1f5ce3f5
Brad Rhoads <bdrhoa@gmail.com>
2023-03-15 17:17:10 -0600

    Update AppDelegate.mm
    Added Objective-C collector code.
    
    Currently failing on all lines references KDataCollector.

--- ios/ReactNativeKountDemo/AppDelegate.mm
+++ ios/ReactNativeKountDemo/AppDelegate.mm
@@ -35,8 +35,41 @@
 
 //function added manually per https://developer.kount.com/hc/en-us/articles/4411084422420 for iOS 12
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-  KountAnalyticsViewController *backgroundTaskObject = [[KountAnalyticsViewController alloc] init];
+    KountAnalyticsViewController *backgroundTaskObject = [[KountAnalyticsViewController alloc] init];
     [backgroundTaskObject registerBackgroundTask];
 }
+
+
+
+NSString *sessionID = nil;
+
+
+[[KDataCollector sharedCollector] setDebug:YES];
+
+// Set your Merchant ID
+
+[[KDataCollector sharedCollector] setMerchantID:99999];
+// Set the location collection configuration
+[[KDataCollector sharedCollector] setLocationCollectorConfig:KLocationCollectorConfigRequestPermission];
+// For a released app, you'll want to set this to KEnvironmentProduction
+[[KDataCollector sharedCollector] setEnvironment:KEnvironmentTest];
+[[KountAnalyticsViewController sharedInstance] setEnvironmentForAnalytics: [KDataCollector.sharedCollector environment]];
+//To collect Analytics Data, set Boolean flag - analyticsData to YES.
+BOOL analyticsData = YES;
+[[KountAnalyticsViewController sharedInstance] collect:sessionID analyticsSwitch:analyticsData completion:^(NSString * _Nonnull sessionID, _Bool success, NSError * _Nullable error) {
+  //Completion block to know whether device data collection is successful/failed with error/failed without error.
+  if (success) {
+    NSLog(@"Collection Successful");
+  }
+  else {
+    if(error != nil) {
+      NSLog(@"Collection failed with error:%@",error.description);
+    }
+    else {
+      NSLog(@"Collection failed without error");
+    }
+  }
+}];
+
 
 @end
