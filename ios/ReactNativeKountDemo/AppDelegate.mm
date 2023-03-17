b6552162b546019ef316db157d23deff5a7f1864
Brad Rhoads <bdrhoa@gmail.com>
2023-03-16 18:50:46 -0600

    Update AppDelegate.mm
    Changed code to match regular Objective-C example at https://raw.githubusercontent.com/Kount/kount-ios-sdk/1e7b2ef62d4500e772eded27a0f0304fbfe6ae2c/CheckoutExampleObjC/CheckoutExampleObjC/AppDelegate.m.
    
    applicationDidEnterBackground does not seem to fire, but maybe that's because of the simulator.

--- ios/ReactNativeKountDemo/AppDelegate.mm
+++ ios/ReactNativeKountDemo/AppDelegate.mm
@@ -5,26 +5,27 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.moduleName = @"ReactNativeKountDemo";
-    // You can add your custom initial props in the dictionary below.
-    // They will be passed down to the ViewController used by React Native.
-    self.initialProps = @{};
+    //// Configure the Data Collector
     
-    // Configure and collect data using the Kount SDK
     NSString *sessionID = @"123456";
     [[KDataCollector sharedCollector] setDebug:YES];
+    // TODO Set your Merchant ID
     [[KDataCollector sharedCollector] setMerchantID:99999];
+    // TODO Set the location collection configuration
     [[KDataCollector sharedCollector] setLocationCollectorConfig:KLocationCollectorConfigRequestPermission];
+    // For a released app, you'll want to set this to KEnvironmentProduction
     [[KDataCollector sharedCollector] setEnvironment:KEnvironmentTest];
-    [[KountAnalyticsViewController sharedInstance] setEnvironmentForAnalytics:[KDataCollector sharedCollector].environment];
-    bool analyticsData = YES;
-    [[KountAnalyticsViewController sharedInstance] collect:sessionID analyticsSwitch:analyticsData completion:^(NSString * _Nonnull sessionID, bool success, NSError * _Nullable error) {
-        if (success) {
+    KountAnalyticsViewController *dataCollectionObject = [[KountAnalyticsViewController alloc] init];
+    [dataCollectionObject setEnvironmentForAnalytics: [KDataCollector.sharedCollector environment]];
+    // To collect Analytics Data, you'll want set this analyticsData to YES or else NO
+    BOOL analyticsData = YES;
+    [dataCollectionObject collect:sessionID analyticsSwitch:analyticsData completion:^(NSString * _Nonnull sessionID, BOOL success, NSError * _Nullable error) {
+        if(success) {
             NSLog(@"Collection Successful");
         }
         else {
             if (error != nil) {
-                NSLog(@"Collection failed with error:%@", error.description);
+                NSLog(@"Collection failed with error:%@",error.description);
             }
             else {
                 NSLog(@"Collection failed without error");
@@ -32,18 +33,17 @@
         }
     }];
     
-    return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    return YES;
 }
 
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-{
-#if DEBUG
-    return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
-#else
-    return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+  NSLog(@"Enter applicationDidEnterBackground");
+    if(@available(*, iOS 12.4.7)) {
+      NSLog(@"Calling KountAnalyticsViewController");
+        KountAnalyticsViewController *backgroundTaskObject = [[KountAnalyticsViewController alloc] init];
+        [backgroundTaskObject registerBackgroundTask];
+    }
 }
 
 @end
-
-
